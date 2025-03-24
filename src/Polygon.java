e8c90adea1c57078e5bd3a9915e5a03b859f91fa
SZ <s.z.m.francja@gmail.com>
2025-03-24 15:13:56 +0100

    lab4

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,16 +1,52 @@
 import java.util.ArrayList;
 
-public class Polygon {
+public class Polygon extends Shape {
+
+
     private ArrayList<Point> points;
 
-    public Polygon(ArrayList<Point> points) {
-        this.points = points;
-    }
 
     @Override
     public String toString() {
         return "Polygon{" +
                 "points=" + points +
                 '}';
+    }
+
+    public String toSvg(){
+        String beginning = "<svg height=\"220\" width=\"500\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+                "  <polygon points=\"";
+
+
+        String midlle = "";
+        for (Point p : points){
+            midlle += p.getX() + "," +p.getY() + " ";
+        }
+
+        String ending =        "\" " +
+                gangnam.toSvg() +
+                "\" />\n" +
+                "</svg>";
+        return beginning + midlle + ending;
+    }
+
+
+    public Polygon(ArrayList<Point> points) {
+        super();
+        this.points = points;
+    }
+    public Polygon(ArrayList<Point> points, Style gangnam) {
+        super(gangnam);
+        this.points = points;
+    }
+
+    public static Polygon createSquare(Segment s, Style style){
+        ArrayList<Point> points = new ArrayList<>();
+        points.add(s.getStart());
+        points.add(s.getEnd());
+        //Segment perpendicularSegment = s.findPerpendicular();
+        //points.add(perpendicularSegment.getStart());
+        //points.add(perpendicularSegment.getEnd());
+        return new Polygon(points,style);
     }
 }
---
+++ src/Style.java
@@ -0,0 +1,44 @@
+public class Style {
+    private String fillColor;
+    private String strokeColor;
+    private double strokeWidth;
+
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    public void setFillColor(String fillColor) {
+        this.fillColor = fillColor;
+    }
+
+    public String getStrokeColor() {
+        return strokeColor;
+    }
+
+    public void setStrokeColor(String strokeColor) {
+        this.strokeColor = strokeColor;
+    }
+
+    public double getStrokeWidth() {
+        return strokeWidth;
+    }
+
+    public void setStrokeWidth(double strokeWidth) {
+        this.strokeWidth = strokeWidth;
+    }
+
+    public Style(String fillColor, String strokeColor, double strokeWidth) {
+        this.fillColor = fillColor;
+        this.strokeColor = strokeColor;
+        this.strokeWidth = strokeWidth;
+    }
+
+    public String toSvg(){
+        return "style=\"fill:" +
+                fillColor +
+                ";stroke:" +
+                strokeColor +
+                ";stroke-width:" +
+                strokeWidth;
+    }
+}
